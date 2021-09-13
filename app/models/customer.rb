12e0c26b9a989a1caaa8c8c2ac82c09b9ea1a523
Sam Roble <smroble1996@gmail.com>
2021-09-13 12:52:52 -0600

    I hope this one is good enough

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,3 +6,4 @@ def create_order(customer, num_items)
     Order.create(status: status, num_items: num_items, customer_id: id, product_id: product.id)
 end
 
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,20 +1,19 @@
 class Product < ActiveRecord::Base
     has_many :orders
     has_many :customers, through: :orders
-end
 
-def create_order
- "#{customer.id} #{num_items}"
-end
-
-def print_all_invoices
-
-end
-
-def number_ordered
-
-end
-
-def volume
-
+    def create_order
+        "#{customer.id} #{num_items}"
+       end
+       
+       def print_all_invoices
+       
+       end
+       
+       def number_ordered
+       
+       end
+       
+       def volume
+       
 end
